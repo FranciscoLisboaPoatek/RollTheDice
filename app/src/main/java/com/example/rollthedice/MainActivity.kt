f64e8cf039a046ccd87db2aa94c6f7fc763d548e
Francisco Lisboa <francisco.lisboa@poatek.com>
2023-07-21 15:25:09 -0300

    Dice are now images, diceRoll unit test

--- app/src/main/java/com/example/rollthedice/MainActivity.kt
+++ app/src/main/java/com/example/rollthedice/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.rollthedice
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 /**
  * This activity allows the user to roll a dice and view the result
@@ -13,20 +13,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.button);
+        rollDice();
         rollButton.setOnClickListener { rollDice() }
     }
     /**
      * Roll the dice and update the screen with the result.
      */
     private fun rollDice() {
-        val dice = Dice();
-        var diceRoll = dice.rollDice();
         // Updates screen with the number rolled
-        val resultTextView1: TextView = findViewById(R.id.dice1);
-        val resultTextView2: TextView = findViewById(R.id.dice2);
-        resultTextView1.text = diceRoll.toString();
-        diceRoll = dice.rollDice()
-        resultTextView2.text = diceRoll.toString();
+        val dice = Dice();
+
+        val diceView1 : ImageView = findViewById(R.id.dice1);
+        val diceView2 : ImageView = findViewById(R.id.dice2);
+
+        var diceRoll1 = dice.rollDice();
+        var diceRoll2 = dice.rollDice();
+
+        //Finds image by a String
+        var resId = this.getResources().getIdentifier("dice_$diceRoll1", "drawable", this.getPackageName());
+        diceView1.setImageResource(resId);
+
+        resId = this.getResources().getIdentifier("dice_$diceRoll2", "drawable", this.getPackageName());
+        diceView2.setImageResource(resId);
+
     }
 }
 
